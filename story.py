aa604755465622619a1995812a73be6fe00cc752
MahonriM <cripto3000z@gmail.com>
2022-10-03 22:18:37 -0500

    Create story.py

---
+++ story.py
@@ -0,0 +1,13 @@
+#novel
+#Introduction
+def tellmeastory():
+    print("Narrator: A long time ago, in a world of dragons and beasts when people didn't exist.There was a big war")
+    print("Let's meet our protagonist of this stroy. It is a dragon called Red")
+    print(".........................................................................")
+    Red="I am Red the dragon. Lord of the fire the ultimate emperator"
+    Blue="I am Blue the biggest beast of the planet"
+    print("Red:",Red)
+    print("Blue:",Blue)
+    print("Narrator: A match started between both. Red killed Blue.")
+    print("The end?")
+tellmeastory()
